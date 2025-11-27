5cb5c92fbc478e96379611cb5743a9c6d1888307
agranier <antoine.granier@insystem.fr>
2025-11-27 09:42:03 +0100

    fix

--- proxy.ts
+++ proxy.ts
@@ -7,7 +7,7 @@ acceptLanguage.languages(languages)
 
 export const config = {
   // matcher: '/:lng*'
-  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|favicon.png|sw.js|site.webmanifest|sitemap.xml|robots.txt|.*\\.(?:png|jpg|jpeg|svg|webp|ico|gif)).*)'
+  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|favicon.png|sw.js|site.webmanifest|sitemap.xml|sitemap/sitemap.xml|robots.txt|.*\\.(?:png|jpg|jpeg|svg|webp|ico|gif)).*)'
   ]
 }
 
